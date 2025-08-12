ddd1b32c29e43453e95316e615ae8a76836ee889
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-08-12 15:01:08 +0900

    Change code position to follow instructions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,20 +1,5 @@
 module Enumerable
   # Your code goes here
-end
-
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
-class Array
-  # Define my_each here
-  def my_each
-    for i in self do
-      yield(i)
-    end
-    return self
-  end
-
   def my_each_with_index
     index = 0
 
@@ -92,5 +77,20 @@ class Array
     end
     
     return sum
+  end
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each
+    for i in self do
+      yield(i)
+    end
+    
+    return self
   end
 end
